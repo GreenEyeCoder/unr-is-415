ed242d0438103fbe4d55170ef286832a2a07684b
Joel DeRouchey <joel.derouchey@nevada.unr.edu>
2023-02-06 11:35:16 -0800

    ready

--- assignments-001.py
+++ assignments-001.py
@@ -8,72 +8,45 @@ Print out all three multiples lists, as well as the list of remaining elements, 
 multiple of any of the three. Output the results in ascending order. Feel free to use the 
 inbuilt sorting functions
 
-
-response from chat-gpt
-
 """
 
-import random
-
-# generate a list of 20 random numbers between 1 and 100
-random_numbers = [random.randint(1, 100) for i in range(20)]
-
-# separate multiples of 2, 3, and 6 into separate lists
-multiples_2 = [num for num in random_numbers if num % 2 == 0 and num % 6 != 0]
-multiples_3 = [num for num in random_numbers if num % 3 == 0 and num % 6 != 0]
-multiples_6 = [num for num in random_numbers if num % 6 == 0]
-
-# store the remaining elements that are not a multiple of any of the three
-remaining = [num for num in random_numbers if num % 2 != 0 and num % 3 != 0 and num % 6 != 0]
-
-# sort all the lists in ascending order
-multiples_2.sort()
-multiples_3.sort()
-multiples_6.sort()
-remaining.sort()
-
-# print the lists
-print("Multiples of 2:", multiples_2)
-print("Multiples of 3:", multiples_3)
-print("Multiples of 6:", multiples_6)
-print("Remaining numbers:", remaining)
-
-
-
-# my attempt at this assignment
-
+print(r"My attempt")
 
 from random import randint
 
 
 #Generate list of 20 random variables
 my_loop = 20
-
-
-rv1 = [] # empty list rv stands for random variables
+rv = [] # empty list rv stands for random variables
 for i in range(my_loop):
         ranNum = randint(1,100)
-        rv1.append(ranNum)
-print("My random numbers           :",'{:04d}'.format(rv1))
-
-
-print("Random numbers from Chat-GPT:",'{:04d}'.format(random_numbers))
-
-rv = random_numbers
-
+        rv.append(ranNum)
 
 #multiples
-for i in [2,3,6]:
+reminders = [] # empty list
+
+for i in [-1,2,3,6]:
         m = [] # empty list, m stands for multiples
         for j in range(my_loop):
+                if  i == -1:
+                    if  rv[j] % 2 != 0 \
+                    and rv[j] % 3 != 0 \
+                    and rv[j] % 6 != 0:
+                        reminders.append(rv[j])
                 if rv[j] % i == 0:
                         if i != 6:
                                 if rv[j] % 6 !=0:
                                         m.append(rv[j])
                         else:
                                 m.append(rv[j])
-        m.sort()                   
-        print(f"Muliples of {i}",m)
+        m.sort()
+        if i != -1:
+            print(f"Muliples of {i}",m)
+
+reminders.sort()
+print(f"Remainders: {reminders}")
+
+
 
 
 
--- working-file-assignment-01.py
+++ working-file-assignment-01.py
@@ -85,29 +85,5 @@ print(f"Remaining number list:{remainingNumbers}")
 print(f"List of duplicates that where selected: {duplicates}")
 
 
-print(r'[IS 615 only]')
 
-import random
 
-# generate a list of 20 unique random numbers between 1 and 100
-random_numbers = list(set([random.randint(1, 100) for i in range(20)]))
-
-# separate multiples of 2, 3, and 6 into separate lists
-multiples_2 = [num for num in random_numbers if num % 2 == 0 and num % 6 != 0]
-multiples_3 = [num for num in random_numbers if num % 3 == 0 and num % 6 != 0]
-multiples_6 = [num for num in random_numbers if num % 6 == 0]
-
-# store the remaining elements that are not a multiple of any of the three
-remaining = [num for num in random_numbers if num % 2 != 0 and num % 3 != 0 and num % 6 != 0]
-
-# sort all the lists in ascending order
-multiples_2.sort()
-multiples_3.sort()
-multiples_6.sort()
-remaining.sort()
-
-# print the lists
-print("Multiples of 2:", multiples_2)
-print("Multiples of 3:", multiples_3)
-print("Multiples of 6:", multiples_6)
-print("Remaining numbers:", remaining)
